12240330560d5f7722ec736352a38338492f6943
aboynejames <aboynejames@gmail.com>
2025-12-13 16:28:13 +0000

    remove and startover

--- nativescript.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NativeScriptConfig } from '@nativescript/core';
-export default {
-  id: 'org.healthscience.healthcues_bb',
-  appPath: 'src',
-  appResourcesPath: 'App_Resources',
-  android: { v8Flags: '--expose_gc', markingMode: 'none' },
-  bundler: 'vite'
-} satisfies NativeScriptConfig;
